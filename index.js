08cb3443714204d6ff316a0e66daecf7576f16e8
Garrett Cox <garrettjcox@gmail.com>
2015-12-11 10:12:12 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports.pfy = function(method, noError) {
+  return function() {
+    var args = Array.from(arguments);
+    return new Promise((resolve, reject) => {
+      method.apply(this, args.concat((err, res) => {
+        if (noError && err) resolve(err);
+        else if (err) reject(err);
+        else resolve(res);
+      }));
+    });
+  };
+};
